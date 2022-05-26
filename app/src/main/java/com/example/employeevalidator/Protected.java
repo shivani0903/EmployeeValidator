e416b74b5828f53d40d93b7fc84addf99fa938b8
Shivangi <shuklashivangi0903@gmail.com>
2022-05-26 16:07:52 +0530

    New pages added

---
+++ app/src/main/java/com/example/employeevalidator/Protected.java
@@ -0,0 +1,14 @@
+package com.example.employeevalidator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Protected extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_protected);
+    }
+}
